a2d0783733b03512d3742f9cd9a8b8f06dbd7542
BerettaSM <ramonsaviato@hotmail.com>
2023-07-19 00:05:31 -0300

    feat: Add basic operations
    Add basic calculator operations and display logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,2 +1,27 @@
-import Calculator from "./calculator";
+import Calculator from "./calculator.js";
 
+const calculator = new Calculator()
+
+const buttons = document.querySelector('.buttons')
+const display = document.querySelector('.display')
+const current = document.querySelector('.current')
+const previous = document.querySelector('.previous')
+const operation = document.querySelector('.operation')
+
+buttons.addEventListener('click', onClick)
+
+function onClick(event) {
+    if(event.target.tagName !== 'BUTTON') return
+
+    const { textContent: value } = event.target
+    
+    calculator.compute(value)
+
+    updateDisplay()
+}
+
+function updateDisplay() {
+    current.textContent = calculator.current
+    previous.textContent = calculator.previous
+    operation.textContent = calculator.operation
+}
--- assets/scripts/calculator.js
+++ assets/scripts/calculator.js
@@ -1,36 +1,87 @@
-export default class Calculator {
-    
-    static operations = {
-        '+':   this.add,
-        '-':   this.subtract,
-        '×':   this.multiply,
-        '÷':   this.divide,
+class Calculator {
+    operations = {
+        '+': this.add,
+        '-': this.subtract,
+        '×': this.multiply,
+        '÷': this.divide,
         '+/-': this.invertSignal,
-        '%':   this.percent
-    }
+        '%': this.percent,
+    };
 
-    static add(n1, n2) {
+    add(n1, n2) {
         return n1 + n2;
     }
 
-    static subtract(n1, n2) {
+    subtract(n1, n2) {
         return n1 - n2;
     }
 
-    static multiply(n1, n2) {
+    multiply(n1, n2) {
         return n1 * n2;
     }
 
-    static divide(n1, n2) {
-        return n1 - n2;
+    divide(n1, n2) {
+        return n1 / n2;
     }
 
-    static invertSignal(n) {
+    invertSignal(n) {
         return n * -1;
     }
 
-    static percent(n1, n2) {
-        return n1 / 100 * n2;
+    percent(n1, n2) {
+        return (n1 / 100) * n2;
     }
 
+    operate(n1, symbol, n2) {
+        const operation = this.operations[symbol];
+        if (!operation) {
+            throw Error('Unsupported operation.');
+        }
+        return operation(n1, n2);
+    }
+}
+
+export default class StatefulCalculator extends Calculator {
+    constructor() {
+        super();
+        this.current = '0';
+        this.previous = '';
+        this.operation = '';
+    }
+
+    clear() {
+        this.current = '0';
+        this.previous = '';
+        this.operation = '';
+    }
+
+    compute(key) {
+        if (isFinite(key)) {
+            // A number
+            this.current === '0'
+                ? (this.current = key)
+                : (this.current += key);
+        } else if (key === '=') {
+            const n1 = +this.previous;
+            const n2 = +this.current;
+            const symbol = this.operation
+            const result = this.operate(n1, symbol, n2)
+            this.clear()
+            this.current = result
+        } else if (key === 'AC') {
+            this.clear();
+        } else if(key === '+/-') {
+            console.log('invert signal')
+        } else if (key in this.operations) {
+            if(this.current !== '0')
+                this.previous = this.current
+            this.operation = key
+            this.current = '0'
+        } else if (key === '.') {
+            if(!this.current.includes('.'))
+                this.current += '.';
+        } else {
+            throw Error('Unknown key');
+        }
+    }
 }
